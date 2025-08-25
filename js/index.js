61e0a02efb455f9d801cca7858ab4b68f838a9a9
kosterseb <sebkos.code@gmail.com>
2025-08-25 13:04:13 +0200

    made right callbacks and setup for making the skeleton ready before CSS work

--- js/index.js
+++ js/index.js
@@ -1,17 +1,84 @@
 // slå dig løs her... 
 
 const wrapper = document.querySelector(".wrapper");
+const header = document.querySelector(".header");
 
+handleHero();
+handleServices();
+handleFacilities();
+handleSites();
+handleAdvantages();
+handleFooter();
 
-
-
-
-function handlePageLoad() {
-    document.querySelector("wrapper");
-    
-
-
+function handleHero() {
+const heroSection = document.querySelector(".hero");
+heroSection.innerHTML = `
+<div class="hero-content">
+<img src="${hero.image}" alt="Hero image">
+      <div class="hero-text">
+        <h1>${hero.headline}</h1>
+        <p>${hero.copy}</p>
+        <img src="${hero.icon}" alt="icon">
+      </div>
+    </div>
+  `;
 }
 
+function handleServices() {
+const servicesSection = document.querySelector(".services");
+servicesSection.innerHTML = services.map(service => `
+    <div class="service">
+      <img src="${service.img}" alt="${service.name}">
+      <h3>${service.headline}</h3>
+      <p>${service.text}</p>
+    </div>
+  `).join('');
+}
+
+function handleFacilities() {
+const facilitiesSection = document.querySelector(".facilities");
+facilitiesSection.innerHTML = `
+    <h2>${facilities.headline}</h2>
+    <div class="facility-options">
+      ${facilities.options.map(option => `
+        <div class="facility-option">
+          <img src="${option.icon}" alt="${option.headline}">
+          <h3>${option.headline}</h3>
+          <p>${option.text}</p>
+        </div>
+      `).join('')}
+    </div>
+  `;
+}
+
+function handleSites() {
+const sitesSection = document.querySelector(".sites");
+sitesSection.innerHTML = `
+    <h2>${sites.headline}</h2>
+    <p>${sites.text}</p>
+    <div class="site-places">
+      ${sites.places.map(place => `
+        <div class="site-place">
+          <img src="${place.img}" alt="${place.name}">
+          <h3>${place.name}</h3>
+          <p>${place.city}</p>
+        </div>
+      `).join('')}
+    </div>
+    <button class="site-btn">
+      Explore all sites
+      <img src="${sites.btnicon}" alt="arrow icon">
+    </button>
+  `;
+}
+
+function handleAdvantages() {
+const advantagesSection = document.querySelector(".advantages");
+advantagesSection.innerHTML = advantages.map(advantage => `
+    <div class="advantage">
+      <img src="${advantage.icon}" alt="${advantage.headline}">`)
+}
+
+function handleFooter() {}
 //eksempel på at udskrive alle overskrifter i services i konsollen:
-services.forEach(service => console.log(service.headline))
+// services.forEach(service => console.log(service.headline))
